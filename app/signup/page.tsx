73bd91968e05ca99a6ca7e65fbd99d6ea9d9f954
Kolade Faith <faithkolade0507@gmail.com>
2026-03-21 17:29:15 +0100

    Add SignUp component with placeholder content

---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit rerum ea
+      officiis exercitationem veniam? Dicta obcaecati tempora repellat odio, rem
+      quibusdam, quae, consectetur voluptatibus voluptas provident fuga
+      reiciendis error quam?
+    </div>
+  );
+};
+
+export default SignUp;
